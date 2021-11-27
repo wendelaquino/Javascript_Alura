2a42eaf3bd327ed12bc5adab66c7f0a92e7f02e4
Wendel Aquino <wendelaquino@outlook.com>
2021-11-27 11:46:09 -0300

    comittasso

---
+++ Aula01_Javascript.js
@@ -0,0 +1,9 @@
+const meunumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+const hello = 'Hello World !!'
+console.log(hello);
