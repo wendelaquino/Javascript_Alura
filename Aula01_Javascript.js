f63e69588101160e8ad0e752f505b07af219aee7
Wendel Aquino <wendelaquino@outlook.com>
2021-11-27 13:33:03 -0300

    A testar

--- Aula01_Javascript.js
+++ Aula01_Javascript.js
@@ -2,7 +2,7 @@ const meunumero = 3;
 
 const primeiroNumero = 1;
 const segundoNumero = 2;
-
+const testeNumero = "É numero?"
 const operacaoMatematica = primeiroNumero / segundoNumero;
 
 const hello = 'Hello World !!'
