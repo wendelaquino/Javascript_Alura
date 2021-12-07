24a1f7956cd6eb8720ca6befb0dda4316c206641
Wendel Aquino <wendelaquino@outlook.com>
2021-12-07 12:35:50 -0300

    finalizando curso javascript para back end

---
+++ cliente-poup.js
@@ -0,0 +1,27 @@
+class Cliente {
+    constructor(nome,email,cpf,saldo){
+        this.nome = nome
+        this.email = email
+        this.cpf = cpf
+        this.saldo = saldo
+    }
+
+    depositar(valor){
+        this.saldo += valor
+    }
+
+    exibirSaldo(){
+        console.log(this.saldo)
+    }
+}
+
+class ClientePoupanca extends Cliente{
+    constructor(nome,email,cpf,saldo, saldoPoupanca){
+        super(nome,email,cpf,saldo)
+        this.saldoPoupanca = saldoPoupanca
+    }
+    depositarPoupanca(valor){
+        this.saldoPoupanca += valor
+    }
+}
+
---
+++ objetos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: "Wendel",
+    idade: 36,
+    cpf: "12543412321",
+    email: "wendel@email.com"
+}
+
+ console.log(cliente);
+ cliente.fone = "2131241123";
+ console.log(cliente);
