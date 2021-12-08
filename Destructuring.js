d327df121a5a2f14210ccc39f0aee5c99912e76c
Wendel Aquino <wendelaquino@outlook.com>
2021-12-08 02:06:10 -0300

    Destructuring.

--- Destructuring.js
+++ Destructuring.js
@@ -6,9 +6,40 @@ const numerosImpares = [1,3,5]
 
 const numeros = [...numerosPares, ...numerosImpares] // extraindo os dados do array
 
+console.log(...numeros)
+
 // const num1 = 1
 // const nume2 = 2
 // É igual a =
-const num1, num2 = [1,2]
 
-console.log(numeros)
+const [num3, num4] = [1,2]
+
+console.log(num3,num4)
+
+const [num1, num2, ...outrosNumeros] = [1,2,3,4,5,6,7]
+
+console.log(...outrosNumeros)
+
+const [nome1 = 'Ju'] = [1]
+
+console.log(nome1)
+
+const [nome2 = 'Ju'] = []
+
+console.log(nome2)
+
+const pessoa = {
+    nome: 'Ju',
+    idade: 25
+}
+
+const pessoaComTelefone = {...pessoa, telefone: 1234453434}
+
+function impremeDados(dados){
+    const {nome, idade} = dados
+    console.log(nome, idade)
+}
+
+function impremeDados ({nome, idade}) {
+    console.log(nome,idade)
+}
